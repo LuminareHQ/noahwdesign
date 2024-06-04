90c79e37b3ad6a24b0ce3165b20a2a04b44b8816
LuminareHQ <70502610+LuminareHQ@users.noreply.github.com>
2024-06-03 21:49:42 -0400

    updated home image reactivity

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
     <div class="flex flex-col w-full h-full relative overflow-hidden">
         <!--Prevent Interaction with the Iframe-->
         <a href="https://ossiaperformance.com" class="flex flex-col w-full h-full relative overflow-hidden">
-            <img src="/ossia-performance-homepage.png" alt="" class="flex w-full h-full object-contain">
+            <img src="/ossia-performance-homepage.png" alt="" class="flex w-full h-full object-cover">
         </a>
         <!--        <div class="top-0 right-0 bottom-0 left-0 z-10 absolute"></div>-->
         <!--        <iframe src="https://ossiaperformance.com" frameborder="1" style="visibility:hidden;" onload="this.style.visibility = 'visible';" class="flex w-full h-full bg-black"></iframe>-->
