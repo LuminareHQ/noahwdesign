31d8290d5e8f601c0d1002dc722abc780bd8bc18
Noah Weber <noahweber47@hotmail.com>
2025-05-02 21:29:09 -0400

    Update +page.svelte

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -7,9 +7,9 @@
         <p>Developer with an Artists Touch.</p>
     </div>
     <div class="flex flex-col w-full gap-4">
-        <h2 class="text-2xl font-bold">Experience</h2>
+        <h2 class="text-3xl font-bold">Experience</h2>
         <ResumeItem
-                header="Cambrio"
+                header="SigmaNEST (Formerly Cambrio)"
                 location="Cincinnati, Ohio"
                 title="Post Developer"
                 dates="April 2023 - PRESENT"
@@ -19,10 +19,10 @@
                 location="Cincinnati, Ohio"
                 title="Applications Engineer"
                 dates="September 2022 - April 2023"
-                description="Provided technical assistance to customers utilizing the SigmaNEST suite of software tools, and developed internal tools that refined team workflows and increased team efficiency and collaboration."/>
+                description="Provided technical assistance to customers using the SigmaNEST suite of software, and developed internal tools that reduced redundant tasks and increased team efficiency and collaboration."/>
     </div>
     <div class="flex flex-col w-full gap-4">
-        <h2 class="text-2xl font-bold">Education</h2>
+        <h2 class="text-3xl font-bold">Education</h2>
         <ResumeItem
                 header="University of Kentucky"
                 location="Lexington, KY"
@@ -31,57 +31,59 @@
                 description="Bachelor of Science in Digital Media and Design with a focus on Interactive Development (Web, Desktop, VR/Game Development)"/>
     </div>
     <div class="flex flex-col w-full gap-4">
-        <h2 class="text-2xl font-bold">Projects</h2>
+        <h2 class="text-3xl font-bold">Projects</h2>
         <ResumeItem
-                hideComma="true"
-                header="Ossia Performance"
-                location=""
-                title="Co-Founder, Lead Developer"
-                dates="February 2024 - Present"
-                description="Music Technology Startup using SvelteKit, Node.js, MariaDB, and Stripe on AWS and Cloudflare cloud infrastructure."/>
-        <ResumeItem
-                hideComma="true"
-                header="Open Marching"
-                location=""
-                title="Founder"
+                header="SigmaSidekick"
+                location="SigmaNEST Internal Tooling"
+                title="Lead Developer"
                 dates=""
-                description="Developing open source software for the marching arts, including an open source drill writing/ visualization software bundle called DotFlow."/>
+                description="Developed an internal suite of software tools using Tauri, to assist the Operations Department with their everyday tasks."/>
+        <ResumeItem
+                header="Ossia Performance"
+                location="Music Technology Startup"
+                title="Co-Founder, Lead Developer"
+                dates=""
+                description="Music Technology Web-based startup for performing arts groups, utilizing SvelteKit, PostgreSQL, Redis, various AWS services, hosted on Railway."/>
+        <ResumeItem
+                header="DotFlow"
+                location="Open-Source Marching Drill Software"
+                title="Lead Developer"
+                dates=""
+                description="Developing an open source marching drill software, utilizing SQLite, Svelte, ThreeJS, Tauri, and Python for scripting."/>
     </div>
     <div class="flex flex-col w-full gap-4">
-        <h2 class="text-2xl font-bold">Skills</h2>
+        <h2 class="text-3xl font-bold">Languages</h2>
+        <ul class="list-disc list-inside">
+            <li>SvelteKit</li>
+            <li>TypeScript</li>
+            <li>Python</li>
+            <li>HTML/CSS</li>
+            <li>SQL</li>
+            <li>Rust</li>
+            <li>Go</li>
+            <li>C#</li>
+            <li>Object Pascal</li>
+        </ul>
+    </div>
+    <div class="flex flex-col w-full gap-4">
+        <h2 class="text-3xl font-bold">Skills</h2>
         <ul class="list-disc list-inside">
             <li>Cross-Browser Compatible, Responsive, and ARIA Compliant Design</li>
-            <li>Database Administration</li>
+            <li>SQL & No-SQL Database Administration</li>
             <li>Azure DevOps & Git VCS</li>
             <li>API Design & Integration</li>
             <li>Scalable full-stack infrastructure and Containerization</li>
         </ul>
     </div>
-    <div class="flex flex-col w-full gap-4">
-        <h2 class="text-2xl font-bold">Languages</h2>
-        <ul class="list-disc list-inside">
-            <li>SvelteKit</li>
-            <li>TypeScript</li>
-            <li>HTML/CSS</li>
-            <li>Python</li>
-            <li>Go</li>
-            <li>SQL</li>
-            <li>C#</li>
-            <li>Object Pascal</li>
-        </ul>
-    </div>
     <div class="flex flex-col w-full gap-4 max-w-2xl">
-        <h2 class="text-2xl font-bold">Achievements</h2>
-        <ul class="list-disc list-inside">
-            <li>
-                Fast-tracked to Post Developer in 6 months by exceeding expectations. Developed internal tools that
-                improved efficiency and cohesion throughout departments.
-            </li>
-            <li>
-                Collaboratively contributed the second-highest number of posts (27 out of 121 YTD), driving a record
-                number of deliveries in Q1, Q2, and Q3 in 2024.
-            </li>
-        </ul>
+        <h2 class="text-3xl font-bold">Achievements</h2>
+        <ResumeItem
+                hideComma="true"
+                header="SigmaSidekick"
+                location=""
+                title="SigmaNEST Internal Tooling"
+                dates=""
+                description="Developed a tool within the SigmaSidekick desktop app that reads in a supplier’s machine parameter PDF file, analyzes and parses out relevant information, and formats it into a SigmaNEST Tech Table from user defined options, reducing a manual multiday process, to around 30 seconds."/>
     </div>
     <div class="py-12">
         <a href="/Weber_Noah_Resume_Public.pdf" class="border-2 p-2" download
